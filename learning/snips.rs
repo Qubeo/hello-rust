a9b6f155298e1c6d29acc2ea0a3e390e6453955a
Qubeo <32247672+Qubeo@users.noreply.github.com>
2019-01-07 23:57:47 +0100

    cleanup

--- learning/snips.rs
+++ learning/snips.rs
@@ -1,8 +1,12 @@
-
-
 // Optim: Vs. cells.clone() a iter_into()?
 // neighbor_matrix = self.cells.iter()
 //    .map(|i| { 0 })// i.clone() as u8 })
 //    .collect();
 
 // Q: How to create identically sized matrix from a matrix (vector) of a different, but compatible (through the repr(u8)) type?
+
+// Stdin usage
+    // let mut guess = String::new();
+    // io::stdin().read_line(&mut guess)
+    //  .expect("Failed to read line");
+    // println!("You guessed: {}", guess);
